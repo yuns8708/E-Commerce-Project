605fa16e7272c576a774fea52ee763b8efc49264
Yuns <yuns8708@gmail.com>
2025-02-28 19:17:26 +0900

    chore : 토큰 만료 시간 상수 키워드 설정

--- src/main/java/com/yuns/e_commerce/security/TokenProvider.java
+++ src/main/java/com/yuns/e_commerce/security/TokenProvider.java
@@ -1,15 +1,11 @@
 package com.yuns.e_commerce.security;
 
-import com.yuns.e_commerce.service.UserService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -21,11 +17,12 @@ public class TokenProvider {
     @Value("${spring.jwt.secret}")
     private String secretKey;
 
+    // 만료 시간
+    // 1hour
+    public static final long TOKEN_VALID = (long) 1000 * 60 * 30;
+
     // 토큰 생성
     public String generateToken(String userId) {
-        // 만료 시간
-        // 1hour
-        long TOKEN_VALID = (long) 1000 * 60 * 30;
         return Jwts.builder()
                 .setSubject(userId)
                 .setIssuedAt(new Date())
